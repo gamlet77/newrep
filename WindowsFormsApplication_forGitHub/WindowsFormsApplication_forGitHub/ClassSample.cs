8f7d2407f0a1df0f8a1ccf43e9c99d839e5f9379
GamletSPB <gamlet77@gmail.com>
2015-03-10 21:32:34 +0300

    играюсь с классом Vertex3d

---
+++ WindowsFormsApplication_forGitHub/WindowsFormsApplication_forGitHub/ClassSample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication_forGitHub
+{
+    /// <summary>
+    /// Класс содержащий точку в 3d-пространстве
+    /// </summary>
+    public class Vertex3d
+    {
+        //поля
+        private double _x;
+        private double _y;
+        private double _z;
+        
+        //свойства
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public double Z
+        {
+            get { return _z; }
+            set { _z = value; }
+        }
+
+        /// <summary>
+        /// Метод, обнуляет все три координаты
+        /// </summary>
+        public void SetToOrigin()
+        {
+            X = Y = Z = 0;
+        }
+    }
+}
--- WindowsFormsApplication_forGitHub/WindowsFormsApplication_forGitHub/Form1.cs
+++ WindowsFormsApplication_forGitHub/WindowsFormsApplication_forGitHub/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WindowsFormsApplication_forGitHub
 {
     public partial class Form1 : Form
@@ -20,6 +21,13 @@ namespace WindowsFormsApplication_forGitHub
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Ай, хорошо нажал!");
+            Vertex3d a = new Vertex3d();
+            a.X = 1;
+            a.Y = 3.4;
+            a.Z = 5.0;
+            MessageBox.Show("X " + a.X.ToString() + "; Y " + a.Y.ToString() + "; Z " + a.Z.ToString() + ";");
+            a.SetToOrigin();
+            MessageBox.Show("X " + a.X.ToString() + "; Y " + a.Y.ToString() + "; Z " + a.Z.ToString() + ";");
         }
     }
 }
